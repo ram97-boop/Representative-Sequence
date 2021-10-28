4d9d1e17a87878cd2331f8b17e0fd501979c04be
Ram Linux <ram97-boop@users.noreply.github.com>
2021-10-28 18:30:51 +0200

    Add a .gitignore and create the function findDistances()

--- findRepresentativeTranscript.py
+++ findRepresentativeTranscript.py
@@ -1,2 +1,15 @@
 from Bio import pairwise2 as pw
+import numpy as np
 
+def findDistances(seq, other_sequences):
+    '''
+    Pairwise-aligns the sequence seq with
+    all the sequences in the other_sequences 
+    and then calculates the distances from
+    seq to all of them.
+
+    Output:
+    distances (list of distances from
+    seq to all sequences in other_sequences)
+    '''
+    pass
