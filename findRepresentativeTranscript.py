0dbf30de49a00f485e0b5a31501a71e835309f36
Ram Linux <ram97-boop@users.noreply.github.com>
2021-10-29 01:04:04 +0200

    Add *.swp in .gitignore. Change findDistances() to findScores() since we're storing alignment scores in the matrix, and it seems to work properly so far.

--- findRepresentativeTranscript.py
+++ findRepresentativeTranscript.py
@@ -15,19 +15,29 @@ class DistanceMatrix:
         for i in range(cols):
             matrix.append([])
 
-        return np.array(matrix, dtype=int)
+        return matrix
 
 
 
-def findDistances(seq, other_sequences):
+def findScores(gene, other_sequences):
     '''
-    Pairwise-aligns the sequence seq with
-    all the sequences in the other_sequences 
-    and then calculates the distances from
-    seq to all of them.
+    Pairwise-aligns the sequences of gene with
+    all of the sequences in other_sequences 
+    and then calculates the scores of gene's
+    sequences' alignments with them.
 
     Output:
-    distances (list of distances from
-    seq to all sequences in other_sequences)
+    scores = matrix of alignment scores from gene's
+    sequences to all sequences in other_sequences.
     '''
-    
+    scores = DistanceMatrix(len(gene), len(other_sequences))
+    seq_i = 0
+
+    for seq1 in gene:
+        for seq2 in other_sequences:
+            alignment = pw.align.globalxx(seq1, seq2)
+            scores.matrix[seq_i].append(alignment[0].score)
+
+        seq_i+=1
+
+    return scores
