84aae63e2a8033e08c276ee26f00b8ffdcc1a620
Ram Linux <ram97-boop@users.noreply.github.com>
2021-11-01 16:41:42 +0100

    Create initial code for a function that calculates edit distances between sequences

--- rep-transc.py
+++ rep-transc.py
@@ -44,6 +44,30 @@ def findScores(gene, otherSequences):
 
     return scores
 
+def findDistances(gene, otherSequences):
+    '''
+    Calculate the edit distances from gene's sequences
+    to the sequences in otherSequences.
+
+    Returns a ScoreMatrix containing the distances
+    between gene's sequences and otherSequences sequences.
+    '''
+    distances = ScoreMatrix(len(gene), len(otherSequences))
+    seq_i = 0 #The index of gene's sequences in the matrix distances.
+
+    for seq1 in gene:
+        differences = 0
+        for seq2 in otherSequences:
+            
+            i = 0
+            while i < min([len(seq1), len(seq2)]): #This loop will iterate up to the length of the shorter sequence.
+                if seq1[i] != seq2[i]:
+                    differences += 1
+
+                i+=1
+        
+            differences += abs(len(seq1) - len(seq2)) #Add in the difference in length
+
 def getSequences(geneFile):
     '''
     Returns a list of the sequences in
@@ -51,26 +75,21 @@ def getSequences(geneFile):
     '''
     gene = open(geneFile, 'r')
     line = gene.readline()
-    sequences = [] # Will contain the sequences of gene.
+    sequences = [] #Will contain the sequences of gene.
     try:
-        seq_i=0 # Will be used to index the list sequences.
+        seq_i=0 #Will be used to index the list sequences.
 
-        # While we're not at the end of the file
-        while line != '':
-            # If we're at the identifier line of a sequence
-            if line[0] == '>':
-                # Add a placeholder for the sequence in
-                # the list sequences
-                sequences.append('')
+        while line != '': #While we're not at the end of the file
+            if line[0] == '>': #If we're at the identifier line of a sequence
 
-                # Go to the next line, which is the start
-                # of the sequence
-                line = gene.readline()
+                sequences.append('') #Add a placeholder for the sequence in the list sequences
+                
+                line = gene.readline() #Go to the next line, which is the start of the sequence
 
-                # While we're not at the end of the sequence
-                while line != '\n':
-                    # Append the line (subsequence) without
-                    # the '\n' character at the end to the
+                while line != '\n': #While we're not at the end of the sequence
+
+                    # Append the line (subsequence), without
+                    # the '\n' character at the end, to the
                     # sequence in sequences[seq_i]
                     if line[-1] == '\n':
                         sequences[seq_i] += line[:-1]
@@ -79,9 +98,11 @@ def getSequences(geneFile):
 
                     line = gene.readline()
                 
-                # line = '\n' so we move to the next line
-                # which will start with '>' and is the
-                # identifier line for the next sequence
+                # line = '\n' after exiting the while loop
+                # so we move to the next line which will
+                # either start with '>' and is the identifier
+                # line for the next sequence, or it will be
+                # '' which is the end of the file.
                 line = gene.readline()
 
                 seq_i += 1
@@ -111,7 +132,7 @@ def getRepSeq(scoreMatrix):
     '''
     scoreSum = []
     for seqScores in scoreMatrix:
-        # Sum the all the scores of the sequence
+        # Sum all the scores of the sequence
         # and put it in scoreSum
         scoreSum.append(sum(seqScores))
 
@@ -122,3 +143,5 @@ def getRepSeq(scoreMatrix):
         if scoreSum[i] > scoreSum[max_i]:
             max_i = i
         i+=1
+
+    return max_i
