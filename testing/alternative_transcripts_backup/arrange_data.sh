9f56171b7481ed26bd717a7012dfe4ce4af16d5d
Ram Linux <ram97-boop@users.noreply.github.com>
2022-01-10 15:50:46 +0100

    Update backup of alternative_transcripts/ directory

--- testing/alternative_transcripts_backup/arrange_data.sh
+++ testing/alternative_transcripts_backup/arrange_data.sh
@@ -1,12 +1,10 @@
-#testing testing
-
 #n=1
 for geneFile in $(ls _iteration_*_cds.fasta)
 	do
 		geneDirectory=$(echo $geneFile | cut -d . -f 1)
 		
-		mkdir geneDirectory &&
-		mv $geneFile geneDirectory/
+		mkdir $geneDirectory &&
+		mv $geneFile $geneDirectory/
 
 #		echo $n
 #		n=$(expr $n + 1)
