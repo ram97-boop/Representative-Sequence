b6e9741d25ee7a1f9eae855904ec93f965c53550
Ram Linux <ram97-boop@users.noreply.github.com>
2022-01-10 14:25:29 +0100

    Add a shell script arrange_data.sh to arrange the simulated data files into directories

---
+++ testing/alternative_transcripts_backup/arrange_data.sh
@@ -0,0 +1,13 @@
+#testing testing
+
+#n=1
+for geneFile in $(ls _iteration_*_cds.fasta)
+	do
+		geneDirectory=$(echo $geneFile | cut -d . -f 1)
+		
+		mkdir geneDirectory &&
+		mv $geneFile geneDirectory/
+
+#		echo $n
+#		n=$(expr $n + 1)
+	done
