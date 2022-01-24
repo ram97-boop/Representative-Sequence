5771750f7f6420ad9b43bcd26d0a2592086779ae
Ram Linux <ram97-boop@users.noreply.github.com>
2022-01-24 16:38:56 +0100

    Let find_rep.sh find the representative transcripts for each iteration in alternative_transcripts/

--- testing/find_rep.sh
+++ testing/find_rep.sh
@@ -1,4 +1,6 @@
 for directory in $(ls -d alternative_transcripts/*cds)
 	do
-		echo "$directory/representatives.fa $directory/ggal.fa $directory/mdom.fa $directory/mmus.fa $directory/hsap.fa $directory/btaus.fa"
+		echo "$directory/representatives.fa $directory/ggal.fa $directory/mdom.fa $directory/mmus.fa $directory/hsap.fa $directory/btaus.fa" |
+		python3 rep_transc.py &&
+		echo "Representatives for $directory extracted."
 	done
