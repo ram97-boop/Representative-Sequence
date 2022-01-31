452c81b92cb0e370ed0b2332579304e62cb60dcb
Ram Linux <ram97-boop@users.noreply.github.com>
2022-01-31 21:37:11 +0100

    Modify extract_genes.sh so that it's suitable for the simulations in the small/ directory.

--- testing/extract_genes.sh
+++ testing/extract_genes.sh
@@ -1,6 +1,8 @@
-for directory in $(ls -d alternative_transcripts/*cds)
+read simulationDirectory
+
+for directory in $(ls -d ${simulationDirectory}/*cds)
 	do
-		#echo "$directory/$(ls $directory) ggal mdom mmus hsap btaus" | python3 extract_genes.py 
-		echo "$(ls $directory/*.fasta) btaus" | python3 extract_genes.py && echo "$directory"
-		#echo "$(ls $directory/*.fasta)"
+		echo "$directory/$(ls $directory) ppan ptro hsap ggor pabe" | python3 extract_genes.py 
+#		echo "$(ls $directory/*.fasta) btaus" | python3 extract_genes.py && echo "$directory"
+		echo "$(ls $directory/*.fasta)"
 	done
