1ad294ef61668186aab1248e34990735b0cf991d
Ram Linux <ram97-boop@users.noreply.github.com>
2022-01-31 21:44:05 +0100

    Modify find_longest.sh so that it takes as input the name of the simulation directory.

--- testing/find_longest.sh
+++ testing/find_longest.sh
@@ -1,6 +1,8 @@
-for directory in $(ls -d alternative_transcripts/*cds)
+read simulationDirectory
+
+for directory in $(ls -d ${simulationDirectory}/*cds)
 	do
-		echo "$directory/longestTranscripts.fa $directory/ggal.fa $directory/mdom.fa $directory/mmus.fa $directory/hsap.fa $directory/btaus.fa" |
+		echo "$directory/longestTranscripts.fa $directory/ppan.fa $directory/ptro.fa $directory/hsap.fa $directory/ggor.fa $directory/pabe.fa" |
 		python3 rep_transc.py &&
 		echo "Longest transcripts of $directory extracted."
 	done
