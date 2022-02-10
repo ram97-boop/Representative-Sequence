b39c10b54d99945c9fa9c47a9bb9f2b8123c6925
Ram Linux <ram97-boop@users.noreply.github.com>
2022-02-10 21:00:24 +0100

    Finish defining functions in sum_of_pairs.py that calculates the sum of pairs of an MSA.

--- testing/sum_of_pairs.py
+++ testing/sum_of_pairs.py
@@ -30,14 +30,18 @@ def scorePair(seq1, seq2):
     indel = -1
     
     score = 0
-    length = len(seq1)
-    for i in range(length):
-        if seq1[i] == seq2[i]:
-            score += match
-        elif (seq1[i] == "-") or (seq2[i] == "-"):
-            score += indel
-        else:
-            score += mismatch
+    for i in range(len(seq1)):
+        if (seq1[i] == seq2[i]):
+            if seq1[i] == "-": #If this column is a gap with no protein residues in either sequence.
+                pass
+            else:
+                score += match
+                
+        else: #If the sequence residues are different at this column.
+            if (seq1[i] == "-") or (seq2[i] == "-"):
+                score += indel
+            else: #If neither sequence has an indel character.
+                score += mismatch
             
     return score
 
@@ -47,6 +51,23 @@ def main():
     inputLine = input()
     inputList = inputLine.split()
     sequenceDictionary = makeMsaDictionary(inputList[1])
+    sequenceList = list(sequenceDictionary.values())
 
     #Create a 5x5 matrix full of zeros.
-    matrix = np.zeros((5,5))
+    # matrix = np.zeros((5,5))
+    
+    sumOfPairs = 0
+
+    for i in range(len(sequenceList) - 1):
+        j = i + 1
+        while j < len(sequenceList):
+            # matrix[i][j] = scorePair(sequenceList[i], sequenceList[j])
+            sumOfPairs += scorePair(sequenceList[i], sequenceList[j])
+            j += 1
+            
+    print(sumOfPairs)
+    
+if __name__ == "__main__":
+    main()
+            
+    
