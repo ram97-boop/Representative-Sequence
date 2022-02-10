5405da52aff9a488ff0893ce12d3851096f58f33
Ram Linux <ram97-boop@users.noreply.github.com>
2022-02-10 16:05:01 +0100

    Add minor changes to find_rep.sh

--- testing/find_rep.sh
+++ testing/find_rep.sh
@@ -1,6 +1,6 @@
 read simulationDirectory
 
-for directory in $(ls -d ${simulationDirectory}/*cds)
+for directory in $(ls -d ${simulationDirectory}/*cds | tail -n $(expr 500 - 482))
 	do
 		echo "$directory/representatives.fa $directory/ppan.fa $directory/ptro.fa $directory/hsap.fa $directory/ggor.fa $directory/pabe.fa" |
 		python3 rep_transc.py &&
