af9ee8d6ce7164ddab4520e83803fb948bf37a1d
Ram Linux <ram97-boop@users.noreply.github.com>
2022-03-15 13:46:44 +0100

    Remove no longer useful scripts

--- src/multiplyAlign.sh
+++
@@ -1,7 +0,0 @@
-for directory in $(ls -d alternative_transcripts/*cds)
-	do
-		echo "$directory" &&
-		echo "Aligning representatives.fa and randomTranscripts.fa" &&
-		clustalo -i "$directory/representatives.fa" -o "$directory/aligned_representatives.fa" &&
-		clustalo -i "$directory/randomTranscripts.fa" -o "$directory/aligned_randoms.fa"
-	done
